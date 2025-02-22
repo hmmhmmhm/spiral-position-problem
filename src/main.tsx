ae720e873a4e93220031d1319f225c7f24dec19f
hmmhmmhm <hmmhmmhm@naver.com>
2025-02-22 23:21:20 +0900

    chore: Relocate tsconfig and integrate src main with app

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from "react-dom/client";
-import App from "./app";
 import "./main.css";
+import { createRoot } from "react-dom/client";
+import CoordinateVisualizer from "./components/coordinate-visualizer";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black p-4">
+      <CoordinateVisualizer />
+    </div>
+  );
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
