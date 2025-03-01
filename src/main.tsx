ff0dbf5bfc87c3cbbac04596f69ee6ab6081339b
hmmhmmhm <hmmhmmhm@naver.com>
2025-03-01 18:18:09 +0900

    chore: Expose coordinates function

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,24 @@
 import "./main.css";
 import { createRoot } from "react-dom/client";
 import SpiralViewer from "./components/spiral-viewer";
+import { useEffect } from "react";
+import { getCoordinates, getNFromCoordinates } from "./lib/gcode/spiral";
+import {
+  calculateCoordinateDiff,
+  reconstructCoordinateDiff,
+} from "./lib/gcode/spherical";
+
+declare let window: any;
 
 function App() {
+  // Expose coordinates function
+  useEffect(() => {
+    window.getCoordinates = getCoordinates;
+    window.getNFromCoordinates = getNFromCoordinates;
+    window.calculateCoordinateDiff = calculateCoordinateDiff;
+    window.reconstructCoordinateDiff = reconstructCoordinateDiff;
+  }, []);
+
   return (
     <div className="min-h-screen bg-black p-4">
       <SpiralViewer />
